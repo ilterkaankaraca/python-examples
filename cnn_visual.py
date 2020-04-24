6d6b7957f17be83568c0efeb4e23b3c69f068c3d
ilterkaankaraca <ilterkaankaraca@gmail.com>
2020-04-24 12:36:13 +0300

    Update cnn_visual.py
    Former-commit-id: 056cd5c86d3c6eac818f9f9942d1d946ce5b0087 [formerly 5a39d5cdb16a0d8cf4c5259bea331b8958ddc5fb]
    Former-commit-id: 6443a347b4aab174d1bc05a6818f47f5a51ad642

--- cnn_visual.py
+++ cnn_visual.py
@@ -7,3 +7,28 @@ import matplotlib.pyplot as plt
 import random
 import os
 
+#sabitlerin belirlenmesi
+image_witdh=128
+image_height=128
+image_size=(image_witdh,image_height)
+image_channels=3
+
+#egitim verisinin hazırlanmaıs
+filenames=os.listdir("E:\Repos\python-examples\dataset\dogs-vs-cats\\train")
+categories = []
+for i in filenames:
+    category = i.split(".")[0]
+    if category == "dog":
+        categories.append(1)
+    else:
+        categories.append(0)
+df = pd.DataFrame({"filename": filenames, "category": categories})
+
+#Veriyi görselleştirmek
+df["category"].value_counts().plot.bar()
+
+
+#ratgele bir görüntü seçilmesi 
+sample = random.choice(filenames)
+image = load_img("E:\Repos\python-examples\dataset\dogs-vs-cats\\train\\"+sample)
+plt.imshow(image)
